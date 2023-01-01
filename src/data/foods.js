ebd655f45165fbfb2696d185aecd8a37c82ecb3c
Shoaib <shoaibshah1255@gmail.com>
2022-12-31 21:31:54 -0800

    Deployed website

--- src/data/foods.js
+++ src/data/foods.js
@@ -1,5 +1,78 @@
 
+import { BrowserRouter as Route} from "react-router-dom"
 
+// product route link
+import Pizza from "../pages/fast/Pizza";
+import Pizza2 from "../pages/fast/Pizza2";
+
+import Pizza3 from "../pages/fast/Pizza3";
+
+import Pizza4 from "../pages/fast/Pizza4";
+
+import ZingerBurger from "../pages/fast/ZingerBurger";
+
+import BeefBurger from "../pages/fast/BeefBurger";
+
+
+import Pasta from "../pages/main/Pasta";
+
+import Pasta2 from "../pages/main/Pasta2";
+
+import Pasta3 from "../pages/main/Pasta3";
+
+import GrilledSteak from "../pages/main/GrilledSteak";
+
+
+import Juice from "../pages/drinks/Juice";
+
+import Juice2 from "../pages/drinks/Juice2";
+
+import Juice3 from "../pages/drinks/Juice3";
+
+import Juice4 from "../pages/drinks/Juice4";
+
+import Juice5 from "../pages/drinks/Juice5";
+
+
+
+import Icecream from "../pages/desserts/Icecream";
+
+import Icecream2 from "../pages/desserts/Icecream2";
+
+import Icecream3 from "../pages/desserts/Icecream3";
+
+import Cake from "../pages/desserts/Cake";
+
+import Cake2 from "../pages/desserts/Cake2";
+
+import Cake3 from "../pages/desserts/Cake3";
+
+// product route link
+
+//category route link
+
+import FastFood from "../pages/fast/FastFood";
+
+import MainDishes from "../pages/main/MainDishes";
+
+import Drinks from "../pages/drinks/Drinks";
+
+import Desserts from "../pages/desserts/Desserts";
+
+//category route link
+
+
+
+
+
+
+
+
+
+
+//category route link
+
+// product picture link
 import pro1 from "../assets/pizza-1.png";
 
 import pro2 from "../assets/dish-1.png";
@@ -42,6 +115,10 @@ import pro20 from "../assets/dessert-5.png";
 
 import pro21 from "../assets/dessert-6.png";
 
+// product picture link
+
+
+
 
 
 
@@ -52,6 +129,10 @@ export const Foods = [
     food_name: "Chilli Pepper Pizza",
     food_category: "Fast Food",
     price: "3",
+    route: <Route exact path="/pizza" element={<Pizza />}></Route>,
+    caroute:
+    <Route exact path="/fast" element={<FastFood />}></Route>
+
     
   },
   {
@@ -60,6 +141,10 @@ export const Foods = [
     food_name: "Chinese Pasta",
     food_category: "Main Dishes",
     price: "3",
+    route:<Route exact path="/pasta" element={<Pasta />}></Route>,
+    caroute:<Route exact path="/dishes" element={<MainDishes />}></Route>
+
+
   },
   {
     id: 3,
@@ -67,6 +152,10 @@ export const Foods = [
     food_name: "Strawberry Juice",
     food_category: "Drinks",
     price: "9",
+    route:<Route exact path="/juice5" element={<Juice5 />}></Route>,
+    
+   caroute:<Route exact path="/drinks" element={<Drinks />}></Route>
+    
   },
   {
     id: 4,
@@ -74,6 +163,9 @@ export const Foods = [
     food_name: "Strawberry Icecream",
     food_category: "Desserts",
     price: "3",
+    route:  <Route exact path="/icecream" element={<Icecream />}></Route>,
+    caroute:<Route exact path="/desserts" element={<Desserts />}></Route>
+
   },
   {
     id: 5,
@@ -81,6 +173,9 @@ export const Foods = [
     food_name: "Bacon Pizza",
     food_category: "Fast Food",
     price: "3",
+    route: <Route exact path="/pizza4" element={<Pizza4 />}></Route>,
+    caroute:
+    <Route exact path="/fast" element={<FastFood />}></Route>
   },
   {
     id: 6,
@@ -88,6 +183,8 @@ export const Foods = [
     food_name: "Grilled Steak",
     food_category: "Main Dishes",
     price: "3",
+    route: <Route exact path="/grilled-steak" element={<GrilledSteak />}></Route>,
+    caroute:<Route exact path="/dishes" element={<MainDishes />}></Route>
   },
   {
     id: 7,
@@ -95,6 +192,8 @@ export const Foods = [
     food_name: "Orange Juice",
     food_category: "Drinks",
     price: "3",
+    route:<Route exact path="/juice" element={<Juice />}></Route>,
+    caroute:<Route exact path="/drinks" element={<Drinks />}></Route>
   },
   {
     id: 8,
@@ -102,6 +201,8 @@ export const Foods = [
     food_name: "Blackout Cake",
     food_category: "Desserts",
     price: "15",
+    route:<Route exact path="/cake" element={<Cake />}></Route>,
+    caroute:<Route exact path="/desserts" element={<Desserts />}></Route>
   },
   {
     id: 9,
@@ -109,6 +210,8 @@ export const Foods = [
     food_name: "Zinger Burger",
     food_category: "Fast Food",
     price: "3",
+    route: <Route exact path="/zinger-burger" element={<ZingerBurger />}></Route>,
+    caroute:<Route exact path="/fast" element={<FastFood />}></Route>
   },
   {
     id: 10,
@@ -116,6 +219,8 @@ export const Foods = [
     food_name: "Italian Pasta",
     food_category: "Main Dishes",
     price: "12",
+    route: <Route exact path="/pasta3" element={<Pasta3 />}></Route>,
+    caroute:<Route exact path="/dishes" element={<MainDishes />}></Route>
   },
   {
     id: 11,
@@ -123,6 +228,8 @@ export const Foods = [
     food_name: "Black Coffee",
     food_category: "Drinks",
     price: "3",
+    route:  <Route exact path="/juice2" element={<Juice2 />}></Route>,
+    caroute:<Route exact path="/drinks" element={<Drinks />}></Route>
   },
   {
     id: 12,
@@ -130,6 +237,9 @@ export const Foods = [
     food_name: "Vanilla Icecream",
     food_category: "Desserts",
     price: "13",
+    route:
+    <Route exact path="/icecream2" element={<Icecream2 />}></Route>,
+    caroute:<Route exact path="/desserts" element={<Desserts />}></Route>
   },
   {
     id: 13,
@@ -137,6 +247,8 @@ export const Foods = [
     food_name: "Mashroom Pizza",
     food_category: "Fast Food",
     price: "7",
+    route:  <Route exact path="/pizza2" element={<Pizza2 />}></Route>,
+    caroute: <Route exact path="/fast" element={<FastFood />}></Route>
   },
   {
     id: 14,
@@ -144,6 +256,8 @@ export const Foods = [
     food_name: "Pakistani Pasta",
     food_category: "Main Dishes",
     price: "5",
+    route: <Route exact path="/pasta2" element={<Pasta2 />}></Route>,
+    caroute:<Route exact path="/dishes" element={<MainDishes />}></Route>
   },
   {
     id: 15,
@@ -151,6 +265,8 @@ export const Foods = [
     food_name: "Pomegranate Juice",
     food_category: "Drinks",
     price: "7",
+    route: <Route exact path="/juice4" element={<Juice4 />}></Route>,
+    caroute:<Route exact path="/drinks" element={<Drinks />}></Route>
   },
   {
     id: 16,
@@ -158,6 +274,8 @@ export const Foods = [
     food_name: "Black Muffin Cake",
     food_category: "Desserts",
     price: "5",
+    route:   <Route exact path="/cake2" element={<Cake2 />}></Route>,
+    caroute:<Route exact path="/desserts" element={<Desserts />}></Route>
   },
   {
     id: 17,
@@ -165,6 +283,9 @@ export const Foods = [
     food_name: "Salami Pizza",
     food_category: "Fast Food",
     price: "10",
+    route:<Route exact path="/pizza3" element={<Pizza3 />}></Route>,
+    caroute:<Route exact path="/fast" element={<FastFood />}></Route>,
+    
   },
   {
     id: 18,
@@ -172,6 +293,8 @@ export const Foods = [
     food_name: "Qulfa Icecream",
     food_category: "Desserts",
     price: "9",
+    route: <Route exact path="/icecream3" element={<Icecream3 />}></Route>,
+    caroute:<Route exact path="/desserts" element={<Desserts />}></Route>
   },
   {
     id: 19,
@@ -179,6 +302,8 @@ export const Foods = [
     food_name: "chezzy hamburger",
     food_category: "Fast Food",
     price: "3",
+    route:  <Route exact path="/beef-burger" element={<BeefBurger />}></Route>,
+    caroute:<Route exact path="/fast" element={<FastFood />}></Route>
   },
   {
     id: 20,
@@ -186,6 +311,8 @@ export const Foods = [
     food_name: "Cheese Cake with Strawberry",
     food_category: "Desserts",
     price: "7",
+    route:    <Route exact path="/cake3" element={<Cake3 />}></Route>,
+    caroute:<Route exact path="/desserts" element={<Desserts />}></Route>
   },
   {
     id: 21,
@@ -193,6 +320,8 @@ export const Foods = [
     food_name: "Lemon Soda",
     food_category: "Drinks",
     price: "2",
+    route:<Route exact path="/juice3" element={<Juice3 />}></Route>,
+    caroute:<Route exact path="/drinks" element={<Drinks />}></Route>
   }
  
 ];
--- src/pages/search/Table.js
+++ src/pages/search/Table.js
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 
 
 const Table = ({ data }) => {
@@ -15,8 +15,8 @@ const Table = ({ data }) => {
           {data.map((item) => (
             <tr key={item.id} style={{ border: '1px solid #ccc' }}>
               <td style={{ border: '1px solid #ccc', padding: '8px' }}><img src={item.food_image}  alt={item.name}   style={{ width: '50%', height: 'auto', margin: '0', marginTop: '10px', marginBottom: '10px' }}   /></td>
-              <td style={{ border: '1px solid #ccc', padding: '8px' }}>{item.food_name}</td>
-              <td style={{ border: '1px solid #ccc', padding: '8px' }}>{item.food_category}</td>
+              <td style={{ border: '1px solid #ccc', padding: '8px' }}> <Link to={item.route.props.path} className="important-link"> {item.food_name} </Link>  </td>
+              <td style={{ border: '1px solid #ccc', padding: '8px' }}> <Link to={item.caroute.props.path} className="important-link"> {item.food_category}</Link></td>
               <td style={{ border: '1px solid #ccc', padding: '8px' }}>${item.price}</td>
             </tr>
           ))}
